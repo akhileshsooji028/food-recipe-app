72e9d9287c9a2ac7f1efb0b723778fd09f87781b
Akhilesh Sooji <akhisooji028@gmail.com>
2020-04-02 10:39:19 +0530

    simple food recipe react app using third party API for fetching and search data

---
+++ src/Recipe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Style from './recipe.module.css'
+
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+
+    return (
+        <div className={Style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <img className={styleMedia.image} src={image} alt="" />
+        </div>
+    );
+};
+
+
+export default Recipe;
